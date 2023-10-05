d871431812c98c6f93e2a193ce84ad516cf3141a
elico <elico@Herbert>
2023-10-04 19:56:57 -0700

    Hangman ALMOST DONE

--- src/_03_Hangman/Hangman.java
+++ src/_03_Hangman/Hangman.java
@@ -20,7 +20,8 @@ public class Hangman implements KeyListener {
 	String word;
 
 	public void showWindow() {
-		String numInput = JOptionPane.showInputDialog("Enter how many rounds you want to play (between 1 and 269)");
+		String numInput = JOptionPane.showInputDialog("Enter how many rounds you want to play (between 1 and "
+				+ Utilities.getTotalWordsInFile("dictionary.txt") + ")");
 		try {
 			num = Integer.parseInt(numInput);
 		} catch (Exception e) {
@@ -28,13 +29,13 @@ public class Hangman implements KeyListener {
 			showWindow();
 			return;
 		}
-		
-		if (num < 1 || num > 269) {
+
+		if (num < 1 || num > Utilities.getTotalWordsInFile("dictionary.txt")) {
 			JOptionPane.showMessageDialog(null, "Enter a valid number!");
 			showWindow();
 			return;
 		}
-		
+
 		for (int i = 0; i < num; i++) {
 			nextValue = Utilities.readRandomLineFromFile("dictionary.txt");
 			if (!words.contains(nextValue)) {
@@ -43,14 +44,14 @@ public class Hangman implements KeyListener {
 				i--;
 			}
 		}
-		
+
 		rounds = 0;
 		word = words.pop();
 		frame = new JFrame();
 		panel = new JPanel();
 		lives = 6;
 		label = new JLabel("");
-		label.setFont(new Font("Serif", Font.PLAIN, 48));
+		label.setFont(new Font("Arial", Font.PLAIN, 48));
 		for (int i = 0; i < word.length(); i++) {
 			label.setText(label.getText() + "_ ");
 		}
@@ -59,9 +60,11 @@ public class Hangman implements KeyListener {
 		frame.add(panel);
 		frame.setTitle("Lives: " + lives);
 		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.addKeyListener(this);
 		frame.setVisible(true);
 		frame.pack();
-		
+
 	}
 
 	public static void main(String[] args) {
@@ -70,29 +73,45 @@ public class Hangman implements KeyListener {
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		if (!label.getText().contains(e.getKeyChar() + "") && e.getKeyChar() != '_' && e.getKeyChar() != ' ') {
-			if (word.contains(e.getKeyChar() + "")) {
-				for (int i = 0; i < word.length() - 1; i++) {
-					if (word.charAt(i) == e.getKeyChar()) {
-						// new StringBuilder()
+		System.out.println(lives);
+		if (!label.getText().contains(e.getKeyChar() + "") && e.getKeyChar() != '_' && e.getKeyChar() != ' ' && word.contains(e.getKeyChar() + "")) {
+				StringBuilder s = new StringBuilder(label.getText());
+				for (int i = 0; i < s.length() - 1; i += 2) {
+					if (word.charAt(i / 2) == e.getKeyChar()) {
+						s.replace(i, i + 1, e.getKeyChar() + "");
+						label.setText(s.toString());
+						frame.pack();
 					}
 				}
-			} else {
-				lives --;
+		} else {
+			lives--;
+			frame.setTitle("Lives: " + lives);
+			if (lives == 0) {
+				// To do: game over screen
 			}
-		} 
+		}
+		if (!label.getText().contains("_")) {
+			try {
+				Thread.sleep(1500);
+			} catch (InterruptedException e1) {
+				e1.printStackTrace();
+			}
+			reset();
+		}
 	}
-	
+
 	public void reset() {
-		if (rounds == num) {
-			
+		frame.dispose();
+		if (rounds == num || words.isEmpty()) {
+			JOptionPane.showMessageDialog(null, "You win! You have completed all " + num + " rounds!");
+			System.exit(0);
 		} else {
 			word = words.pop();
 			frame = new JFrame();
 			panel = new JPanel();
 			lives = 6;
 			label = new JLabel("");
-			label.setFont(new Font("Serif", Font.PLAIN, 28));
+			label.setFont(new Font("Arial", Font.PLAIN, 48));
 			for (int i = 0; i < word.length(); i++) {
 				label.setText(label.getText() + "_ ");
 			}
@@ -101,19 +120,21 @@ public class Hangman implements KeyListener {
 			frame.add(panel);
 			frame.setTitle("Lives: " + lives);
 			frame.setVisible(true);
+			frame.setLocationRelativeTo(null);
+			frame.addKeyListener(this);
 			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			frame.pack();
-			rounds ++;
+			rounds++;
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		
+
 	}
 }
